8b3db179f10d458ca9fcfbeb85775edec56701be
LeninRam0s <djalexander.rmix@gmail.com>
2021-03-27 23:18:38 -0600

    ACTUALIZACION CLASE DATOS

--- src/ConexionDB/clsDatos.java
+++ src/ConexionDB/clsDatos.java
@@ -1,9 +1,15 @@
 package ConexionDB;
 
+import ListaPuntos.Lista;
+
+import javax.print.DocFlavor;
+import java.security.PublicKey;
+import java.util.List;
+
 public class clsDatos {
 
-    int id;
-    String nombre;
+    private int id;
+    private String nombre;
 
     //CONSTRUCTORES
     public clsDatos(){
@@ -13,10 +19,11 @@ public class clsDatos {
         this.nombre = nombre;
     }
 
-    //GETTER AND SETER
+    //GETTER AND SETTER
     public int getId() {
         return id;
     }
+
 
     public void setId(int id) {
         this.id = id;
